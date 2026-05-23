1361d9a88b65e26c546c725b1c96b9de86fee170
huskyth <1622313177@qq.com>
2026-05-23 12:43:33 +0800

    feat: 消除飞机多少影响1

--- onpolicy/envs/drone/test/uav_test.py
+++ onpolicy/envs/drone/test/uav_test.py
@@ -1,16 +1,13 @@
 from pathlib import Path
-
-from onpolicy.scripts.train.train_drone import parse_args
 import sys
-
+import numpy as np
+from onpolicy.scripts.train.train_drone import parse_args
 from onpolicy.envs.drone.mul_uav_env import MultiUavEnv
+from onpolicy.config import get_config
+import configparser
 
 temp = Path(__file__).parent.parent
 sys.path.append(str(temp))
-from onpolicy.config import get_config
-
-"""Train script for MPEs."""
-import configparser
 
 # 读取配置文件中的参数并保存
 config_path = temp / 'config/th_demo.ini'
@@ -20,8 +17,13 @@ cf.read(str(config_path), encoding="utf-8")
 parser = get_config()
 all_args = parse_args(sys.argv[1:], parser)
 env = MultiUavEnv(cf=cf, is_debug=True, episode_limit=100, rank=0)
-import numpy as np
 
 env.reset()
-while not (env.is_terminal[0] and env.is_terminal[1]):
-    env.step(np.array([[0., 0., 0., 0., 1., 0., 0., 0., 0.], [0., 0., 0., 0., 1., 0., 0., 0., 0.]]))
+act = []
+for _ in range(env.n_total_uavs):
+    act.append([0., 0., 0., 0., 1., 0., 0., 0., 0.])
+
+while True:
+    env.step(np.array(act))
+    if all(env.is_terminal):
+        break
